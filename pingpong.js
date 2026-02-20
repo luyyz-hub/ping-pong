25385f63a7d45ac4ae2ce7d6d6a246afce93360f
luyyz-hub <luiz.almeida.ferreira@escola.pr.gov.br>
2026-02-20 07:52:11 -0300

    Create pingpong.js with game setup
    Initialize pingpong game with ball and paddle variables.

---
+++ pingpong.js
@@ -0,0 +1,29 @@
+// Bola 
+let raio = 7.5;
+let xBola = 100;
+let yBola = 200;
+let diametro = raio * 2;
+
+//Velocidade da bola
+let velocidadeXBola = 6;
+let velocidadeYBola = 6;
+
+//raquete
+let xRaquete = 5;
+let yRaquete = 150;
+
+//raquete oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+
+//placar 
+let meusPontos = 0;
+let pontosOponentes = 0;
+
+function setup(){
+  createCanvas(600,400)
+}
+
+function draw(){
+  
+}
