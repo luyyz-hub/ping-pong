2babd8356307c7a79608fd69d94e33c403ee9e45
luyyz-hub <luiz.almeida.ferreira@escola.pr.gov.br>
2026-02-24 10:58:18 -0300

    Add ball movement and paddle drawing functions

--- pingpong.js
+++ pingpong.js
@@ -27,3 +27,13 @@ function setup(){
 function draw(){
   
 }
+function movimentoBolinha(){
+  circle(xBolinha,diametro);
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+  if(xBolinha + raio > width || xBolinha-raio < 0){}
+  if(yBolinha + raio > height || yBolinha-raio < 0){ velocidadeYBolinha *= -1}
+}
+function raquetes(xRaquete,yRaquete){ rect(xRaquete,yRaquete,10,90) }
+
+  
